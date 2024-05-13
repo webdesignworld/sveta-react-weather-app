7416cbfd6c9f719a8fc83261de02e0fbbaa82120
sudaksudak <sveta_kiev_ua@yahoo.com>
2024-05-13 19:40:12 +0200

    added site

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <footer>
           This project was coded by{" "}
           <a
-            href="/"
+            href="https://webdesignworld.co.uk/"
             target="_blank"
             rel="noopener noreferrer"
           >
